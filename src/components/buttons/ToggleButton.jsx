768ff2de70b6e5ac8ac5711082a3b1cef9410e75
PU-MEriksson <malin.eriksson83@icloud.com>
2025-03-20 16:28:39 +0100

    Added toggle button components for registration form

---
+++ src/components/buttons/ToggleButton.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import styles from "./ToggleButton.module.css";
+
+const ToggleButton = ({ label, name, value, onChange }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleChange = () => {
+    setIsChecked(!isChecked);
+    onChange(value); // Sends the value to the parent component
+  };
+
+  return (
+    <label className={`${styles.button} ${isChecked ? styles.selected : ""}`}>
+      <input
+        type="checkbox"
+        name={name}
+        value={value}
+        checked={isChecked}
+        onChange={handleChange}
+        hidden
+      />
+      {label}
+    </label>
+  );
+};
+
+export default ToggleButton;
