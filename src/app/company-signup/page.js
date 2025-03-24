9cab093ba254d823d121c011c58a6d1672888e71
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-24 17:52:44 +0100

    started on page structure

--- src/app/company-signup/page.js
+++ src/app/company-signup/page.js
@@ -1,1 +1,12 @@
-export default function CompanySignup() {}
+import Wrapper from "../../components/layouts/wrapper";
+import InputField from "../../components/input-fields/InputField";
+
+export default function CompanySignup() {
+    return (
+        <Wrapper padding="2.4rem 2.4rem 4rem 2.4rem" gap="2.4rem">
+            <InputField type="text" placeholder="Namn"></InputField>
+            <InputField type="text" placeholder="Vilket företag..."></InputField>
+            <InputField type="mail" placeholder="dittnamn@mail.com"></InputField>
+        </Wrapper>
+    )
+}
