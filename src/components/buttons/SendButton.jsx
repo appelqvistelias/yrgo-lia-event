12b1bfec5a12273124497fc841c826ce6ffdfd3a
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-03-27 11:01:08 +0100

    continued on send button component

--- src/components/buttons/SendButton.jsx
+++ src/components/buttons/SendButton.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 import styles from "./sendButton.module.css";
-//Need styling for the button
 
-// The SendButton component is a button that is used to submit a form.
-// It has a default text of "Skicka" but can be overridden with a custom text.
-// It is styled with the styles.button class.
-const SendButton = ({ children }) => {
+// Used to submit forms
+// Must be placed inside a form
+// Has loading state to prevent user from clicking multiple times
+// Customizable label with "Skicka" as fallback
+
+const sendButton = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <button type="submit" className={styles.Sendbutton}>
-      {children || "Skicka"}
+    <button
+      type="submit"
+      className={styles.sendButton}
+      disabled={isLoading}
+      onClick={() => setIsLoading(true)}
+    >
+      {isLoading ? "Skickar..." : children || "Skicka"}
     </button>
   );
 };
+
+export default sendButton;
