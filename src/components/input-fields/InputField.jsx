203d19014f3322ffcd9fb6acffc2854cb93a2575
PU-MEriksson <malin.eriksson83@icloud.com>
2025-03-27 14:03:16 +0100

    Added onChange and value to inputfield component

--- src/components/input-fields/InputField.jsx
+++ src/components/input-fields/InputField.jsx
@@ -1,10 +1,22 @@
 import styles from "./InputField.module.css";
 
-export default function InputField({ label, type, placeholder }) {
-    return (
-      <label className={styles.label}>
-        { label }
-        <input className={styles.input} type={type} placeholder={placeholder} />
-      </label>
-    );
-  }
+export default function InputField({
+  label,
+  type,
+  placeholder,
+  value,
+  onChange,
+}) {
+  return (
+    <label className={styles.label}>
+      {label}
+      <input
+        className={styles.input}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+}
