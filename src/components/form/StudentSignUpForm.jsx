d05b09a948e5dbdee09f64d6e41dcfa1d1ebfd97
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-01 11:33:31 +0200

    added student signup component and style

---
+++ src/components/form/StudentSignUpForm.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+import { supabase } from "../../../lib/supabase";
+import styles from "./StudentSignUpForm.module.css";
+import ChoiceButton from "../buttons/ChoiceButton";
+import SendButton from "../buttons/SendButton";
+import InputField from "../input-fields/InputField";
+
