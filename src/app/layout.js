6f5cc4733d4a9d36aa57bf2db9e747cf77355766
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-02 16:07:54 +0200

    Added color variables and font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,16 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"], //Add correct variables
+  variable: "--font-base",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
