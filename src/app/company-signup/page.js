3edde3b03dbe96c57c6aae28293770a4d3b5149e
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-02 11:11:44 +0200

    added company signup form

--- src/app/company-signup/page.js
+++ src/app/company-signup/page.js
@@ -1,12 +1,8 @@
-import Wrapper from "../../components/layouts/Wrapper";
-import InputField from "../../components/input-fields/InputField";
+import React from 'react';
+import CompanySignUpForm from '../../components/form/CompanySignUpForm';
 
 export default function CompanySignup() {
     return (
-        <Wrapper padding="2.4rem 2.4rem 4rem 2.4rem" gap="2.4rem">
-            <InputField label="FULLSTÄNDIGT NAMN" type="text" placeholder="Namn"></InputField>
-            <InputField label="FÖRETAG" type="text" placeholder="Vilket företag..."></InputField>
-            <InputField label="E-POST" type="mail" placeholder="dittnamn@mail.com"></InputField>
-        </Wrapper>
+            <CompanySignUpForm />
     )
 }
