4d72074721ef74102f82b56036f8f0af45709b36
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-02 11:09:51 +0200

    removed company signup from landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,2 @@
-import CompanySignUpForm from "@/components/form/CompanySignUpForm";
-import TestForm from "../components/form/TestForm";
-
 export default function Page() {
-  return (
-    <div>
-      <h1>Test</h1>
-      <CompanySignUpForm />
-    </div>
-  );
 }
