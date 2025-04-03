59e35906082fb6b42dd1b87de1017a06c0aabce1
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-03 15:49:56 +0200

    Started on  presentationCard component

---
+++ src/components/cards/PresentationCard.jsx
@@ -0,0 +1,12 @@
+import styles from "./StudentCard.module.css";
+import React from "react";
+
+export default function StudentCard({
+  studentName,
+  education,
+  infoText,
+  image,
+  fieldOfInterest,
+}) {
+  return <div className=""></div>;
+}
