3498a60a335ac3adbe4ef4571e9ad059f73d716c
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-03 10:04:13 +0200

    added basic dev navigation

--- src/app/page.js
+++ src/app/page.js
@@ -1,2 +1,27 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import Wrapper from "../components/layouts/Wrapper";
+
 export default function Page() {
+  return (
+    <Wrapper>
+      <h1>Välkommen!</h1>
+      <ul>
+        <li>
+          <Link href="/student-signup">Student Signup</Link>
+        </li>
+        <li>
+          <Link href="/company-signup">Company Signup</Link>
+        </li>
+        <li>
+          <Link href="/student-login">Student Login</Link>
+        </li>
+        <li>
+          <Link href="/student-dashboard">Student Dashboard</Link>
+        </li>
+      </ul>
+    </Wrapper>
+  );
 }
