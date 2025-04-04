f7235e224840765d9904f7e6e7c5a618d9f4a147
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-04 15:18:00 +0200

    Added inter font to project

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"], //Add correct variables
-  variable: "--font-base",
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>{children}</body>
     </html>
   );
