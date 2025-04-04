8bafd52ca9eb52bfe2f786d862f5de570276e7cb
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-04 14:22:54 +0200

    Deleted filtered-students page

--- src/app/filtered-students/page.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FilteredStudents from "@/components/filters/FilteredStudents";
-
-export default function FilteredStudentsPage() {
-  return <FilteredStudents />;
-}
