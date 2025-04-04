dcc8e4aa06a6874bed0e17d22626be59dfbedbb2
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-04 15:47:24 +0200

    Imported typography to  layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
-import "./globals.css";
+import "@/styles/globals.css";
+import "@/styles/typography.css";
+
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
