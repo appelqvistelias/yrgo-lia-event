c6b10ec183b4d20df03e37b82cf8f8ac5f13a145
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-04 17:01:44 +0200

    Added styling to cardInfo

--- src/components/cards/CardInfo.jsx
+++ src/components/cards/CardInfo.jsx
@@ -1,10 +1,12 @@
 import styles from "./CardInfo.module.css";
 
+// CardInfo component
+// This component is used to display information about the student and company
 export default function CardInfo({ heading, infoText }) {
   return (
     <div className={styles.cardInfoContainer}>
-      <h2 className={styles.heading}>{heading}</h2>
-      <p className={styles.cardInfo}>{infoText}</p>
+      <h2 className={`${styles.heading} title_5`}>{heading}</h2>
+      <p className={`${styles.infoText} sub_2`}>{infoText}</p>
     </div>
   );
 }
