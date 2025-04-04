db31dcf0e80336efcff9c00d3a8b799bd6aeaae3
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-04 14:46:28 +0200

    Started working on components of the student card

--- src/app/students/page.js
+++ src/app/students/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FilteredStudents from "@/components/filters/FilteredStudents";
+import StudentCard from "@/components/cards/StudentCard";
 
 export default function StudentCards() {
   return <FilteredStudents />;
--- src/components/cards/StudentCard.jsx
+++ src/components/cards/StudentCard.jsx
@@ -1,16 +1,22 @@
 import styles from "./StudentCard.module.css";
 import React from "react";
 
-export default function StudentCard({
-  studentName,
-  education,
-  infoText,
-  image,
-  fieldOfInterest = [],
-}) {
-  return (
-    <div className="styles.card">
-      <div className="styles.background"></div>
-    </div>
-  );
+export default function StudentCard(
+  {
+    // studentName,
+    // education,
+    // infoText,
+    // image,
+    // fieldOfInterest = [],
+  }
+) {
+  // Parts of the card:
+  // Icon
+  // Student name
+  // Student information - maybe in heading a paragraph?
+  // Student interests - in a list
+  // Student image
+  // Card background
+
+  return <div className="style.card"></div>;
 }
