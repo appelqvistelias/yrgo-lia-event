09f507b87d24088241ca1442ea41bc7ce7291504
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-05 19:13:13 +0200

    Added CardImage component with styling, and added it to StudentCard

--- src/components/cards/StudentCard.jsx
+++ src/components/cards/StudentCard.jsx
@@ -2,6 +2,7 @@ import styles from "./StudentCard.module.css";
 import StudentName from "./StudentName";
 import CardInfo from "./CardInfo";
 import CardBackground from "./CardBackground";
+import CardImage from "./CardImage";
 
 export default function StudentCard(
   {
@@ -15,7 +16,7 @@ export default function StudentCard(
   // Parts of the card:
   // Icon
   // Student name - KLART!
-  // Student & company information - maybe in heading a paragraph?
+  // Student & company information - KLART!
   // Student & company interests - in a list
   // Student image
   // Card background
@@ -24,6 +25,10 @@ export default function StudentCard(
     <CardBackground>
       <div className={styles.card}>
         <StudentName name="Clara S" program="digital design" />
+        <CardImage
+          imageUrl="https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=2561&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          altText="Student image"
+        />
         <CardInfo
           heading="Digital Designer"
           infoText="Driven designer med passion för typografi och varumärkesbyggande."
