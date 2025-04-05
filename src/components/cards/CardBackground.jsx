0c91d6ebab58395276343374fc88dbab2a02c425
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-05 15:56:14 +0200

    Added CardBackground component

---
+++ src/components/cards/CardBackground.jsx
@@ -0,0 +1,6 @@
+import styles from "./CardBackground.module.css";
+// CardBackground component
+// This component is used to display the background of the card
+export default function CardBackground({ children }) {
+  return <div className={styles.cardBackground}>{children}</div>;
+}
