7b74178ef4b51bb4d250e425663beb52b4898f05
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-08 16:56:50 +0200

    moved components up one layer

--- src/app/company-signup/page.js
+++ src/app/company-signup/page.js
@@ -1,13 +1,6 @@
 import React from "react";
-import Wrapper from "../../components/layouts/Wrapper";
 import CompanySignUpForm from "../../components/form/CompanySignUpForm";
-import HeaderWithLogo from "@/components/layouts/HeaderWithLogo";
 
 export default function CompanySignup() {
-  return (
-    <Wrapper padding="1rem 1.5rem">
-      <HeaderWithLogo>Anmälan</HeaderWithLogo>
-      <CompanySignUpForm />
-    </Wrapper>
-  );
+  return <CompanySignUpForm />;
 }
