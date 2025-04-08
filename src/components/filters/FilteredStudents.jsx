47dfc97beab95572cc49d5fca1e3bfeca24449d1
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-08 12:36:11 +0200

    Removed unused function from FilteredStudents

--- src/components/filters/FilteredStudents.jsx
+++ src/components/filters/FilteredStudents.jsx
@@ -65,13 +65,6 @@ const FilteredStudents = () => {
 
         if (studentsError) throw studentsError;
 
-        // Format the Specializations data from three_d to 3D och replace _ with space
-        // This is a helper function to format specialization names
-        const formatSpecializationName = (name) => {
-          if (name === "three_d") return "3D";
-          return name.replace(/_/g, " ");
-        };
-
         // Format the options for dropdowns
         const formattedPrograms = programsData.map((program) => ({
           id: program.id.toString(),
@@ -100,8 +93,7 @@ const FilteredStudents = () => {
             ss.specialization_id.toString()
           );
           const specializationNames = student.student_specializations.map(
-            (ss) =>
-              formatSpecializationName(ss.specializations.specialization_name)
+            (ss) => ss.specializations.specialization_name
           );
 
           // Return a flattened student object
