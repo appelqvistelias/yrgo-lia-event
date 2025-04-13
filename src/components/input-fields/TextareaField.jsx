200a5b8ddbb15ac42f4455ad35ac18c6686c6ff1
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-13 16:16:55 +0200

    made responsive

--- src/components/input-fields/TextareaField.jsx
+++ src/components/input-fields/TextareaField.jsx
@@ -23,7 +23,7 @@ export default function TextAreaField({
             rows={rows}
             maxLength={maxLength}
           />
-          <p className={styles.paragraph}>Max {maxLength} tecken</p>
+          <p className={styles.maxLengthParagraph}>Max {maxLength} tecken</p>
         </div>
       </label>
       <p className={styles.optionalParagraph} style={{ display: display }}>
