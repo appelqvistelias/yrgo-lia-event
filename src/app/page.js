65a36598216f8fb37f9df1028a706818176e0f15
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-14 11:38:36 +0200

    changed component structure

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,12 @@
 import React from "react";
 import Link from "next/link";
 import MainContainer from "@/components/landing-page/MainContainer";
-import Section from "@/components/landing-page/Section";
-import VideoWindow from "@/components/landing-page/VideoWindow";
+import Hero from "@/components/landing-page/Hero";
 
 export default function Page() {
   return (
     <MainContainer>
-      <Section>
-        <VideoWindow />
-      </Section>
+      <Hero />
     </MainContainer>
   );
 }
