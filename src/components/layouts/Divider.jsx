34e68130ed9c7006236fbca475ddafc09f6b91ce
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-14 15:50:56 +0200

    added color customization

--- src/components/layouts/Divider.jsx
+++ src/components/layouts/Divider.jsx
@@ -1,7 +1,10 @@
 import styles from "./Divider.module.css";
 
-const Divider = ({ margin }) => {
-  return <div className={styles.divider} style={{ margin }} />;
+const Divider = ({
+  margin,
+  borderTop = "1px solid var(--Greyscale-grey1, #121212)",
+}) => {
+  return <div className={styles.divider} style={{ margin, borderTop }} />;
 };
 
 export default Divider;
