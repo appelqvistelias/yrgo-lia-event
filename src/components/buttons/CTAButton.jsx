81c51f5b70965236a4a9dc9789478b554cb55a16
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-14 17:02:04 +0200

    added scroll logic

--- src/components/buttons/CTAButton.jsx
+++ src/components/buttons/CTAButton.jsx
@@ -1,7 +1,18 @@
 import styles from "./CTAButton.module.css";
 
 const CTAButton = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <button className={styles.button} onClick={scrollToBottom}>
+      {children}
+    </button>
+  );
 };
 
 export default CTAButton;
