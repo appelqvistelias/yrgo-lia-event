46221f7b6c85e8c247b8d19069df93b219db12ee
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-15 15:10:53 +0200

    Added default image

--- src/components/cards/StudentCard.jsx
+++ src/components/cards/StudentCard.jsx
@@ -4,13 +4,13 @@ import CardInfo from "./CardInfo";
 import CardBackground from "./CardBackground";
 import CardImage from "./CardImage";
 import CardInterests from "./CardInterests";
-import PropTypes from "prop-types";
+import DefaultImage from "@/icons/yrgologo-big.png";
 
 export default function StudentCard({
   studentName = "Clara S",
   education = "Digital Designer",
   infoText = "Kort text om mig bla bla. Jag gillar söker lia till november etc etc...",
-  image = "https://images.unsplash.com/photo-1580489944761-15a19d654956?q=80&w=2561&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  image = defaultImage, // Default image if no image is provided
   fieldOfInterest = ["UI", "Branding", "3D"],
 }) {
   return (
