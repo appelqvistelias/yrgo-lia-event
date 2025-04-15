ed3a898068d9e22649843409692d56ad6567b15c
PU-MEriksson <malin.eriksson83@icloud.com>
2025-04-15 15:34:25 +0200

    Added default text

--- src/components/cards/StudentCard.jsx
+++ src/components/cards/StudentCard.jsx
@@ -4,21 +4,27 @@ import CardInfo from "./CardInfo";
 import CardBackground from "./CardBackground";
 import CardImage from "./CardImage";
 import CardInterests from "./CardInterests";
-import DefaultImage from "@/icons/yrgologo-big.png";
 
 export default function StudentCard({
-  studentName = "Clara S",
-  education = "Digital Designer",
-  infoText = "Kort text om mig bla bla. Jag gillar söker lia till november etc etc...",
-  image = defaultImage, // Default image if no image is provided
-  fieldOfInterest = ["UI", "Branding", "3D"],
+  studentName = "Namn saknas",
+  education = "Ingen utbildning vald",
+  infoText = "Info-text saknas",
+  image = null,
+  fieldOfInterest = [],
 }) {
+  // Use nullish coalescing to handle the default image
+  const displayImage = image || defaultLogo;
+  console.log("Image value:", displayImage);
+
   return (
     <CardBackground>
       <div className={styles.card}>
         <StudentName name={studentName} program={education} />
         <div className={styles.imageContainer}>
-          <CardImage imageUrl={image} altText={`${studentName}'s profile`} />
+          <CardImage
+            imageUrl={displayImage}
+            altText={`${studentName}'s profile`}
+          />
         </div>
         <div className={styles.lowerHalfContent}>
           <CardInfo heading={education} infoText={infoText} />
