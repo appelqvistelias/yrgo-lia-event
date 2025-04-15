43e2d4f1531265080152b72fc13ded1dfd8ad8f4
Elias Appelqvist <eliasappelqvist@gmail.com>
2025-04-15 15:28:10 +0200

    added navbar

--- src/app/confirmation/company/page.js
+++ src/app/confirmation/company/page.js
@@ -6,23 +6,29 @@ import LayoutParagraph from "@/components/layouts/LayoutParagraph";
 import Wrapper from "@/components/layouts/Wrapper";
 import HeaderWithLogoAndBorder from "@/components/layouts/HeaderWithLogoAndBorder";
 import ButtonsText from "@/components/layouts/ButtonsText";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Confirmation() {
   return (
-    <Wrapper padding="1.5rem" gap="1rem" alignItems="center">
-      <LayoutCard>
-        <HeaderWithLogoAndBorder>TACK FÖR DIN ANMÄLAN!</HeaderWithLogoAndBorder>
-        <LayoutParagraph
-          paragraphText={`Vi ser fram emot att träffa dig ombord på LIA Expressen och ge dig möjlighet att upptäcka framtidens digitala talanger.\n\nTills dess, fundera på vad du letar efter hos en framtida medarbetare.\nHör av dig om du har frågor!`}
-        />
-        <ButtonsText
-          text="Nyfiken på vilka som kommer?"
-          leftButtonText="Studenter"
-          leftButtonLink="/students"
-          rightButtonText="Företag"
-          rightButtonLink="/companies"
-        />
-      </LayoutCard>
-    </Wrapper>
+    <>
+      <Navbar />
+      <Wrapper padding="1.5rem" gap="1rem" alignItems="center">
+        <LayoutCard>
+          <HeaderWithLogoAndBorder>
+            TACK FÖR DIN ANMÄLAN!
+          </HeaderWithLogoAndBorder>
+          <LayoutParagraph
+            paragraphText={`Vi ser fram emot att träffa dig ombord på LIA Expressen och ge dig möjlighet att upptäcka framtidens digitala talanger.\n\nTills dess, fundera på vad du letar efter hos en framtida medarbetare.\nHör av dig om du har frågor!`}
+          />
+          <ButtonsText
+            text="Nyfiken på vilka som kommer?"
+            leftButtonText="Studenter"
+            leftButtonLink="/students"
+            rightButtonText="Företag"
+            rightButtonLink="/companies"
+          />
+        </LayoutCard>
+      </Wrapper>
+    </>
   );
 }
--- src/app/student-signup/page.js
+++ src/app/student-signup/page.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import StudentSignupForm from '../../components/form/StudentSignUpForm';
+import React from "react";
+import StudentSignupForm from "../../components/form/StudentSignUpForm";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function StudentSignup() {
-    return (
-        <StudentSignupForm />
-    )
+  return (
+    <>
+      <Navbar />
+      <StudentSignupForm />
+    </>
+  );
 }
